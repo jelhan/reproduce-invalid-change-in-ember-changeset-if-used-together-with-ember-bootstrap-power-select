cb4c23220a4f21107b80a65d6e6e70ccca177e20
Jeldrik Hanschke <admin@jhanschke.de>
2021-11-28 12:35:53 +0100

    ember-changeset with any nested object

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,6 +2,18 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
 export default class IndexController extends Controller {
+  authorsWithoutEmberData = [
+    { name: 'Jane Doe' },
+    { name: 'John Doe' },
+    { name: 'Max Mustermann' },
+    { name: 'Erika Mustermann' },
+  ];
+
+  postWithoutEmberData = {
+    title: 'Post without Ember Data',
+    author: this.authorsWithoutEmberData[2],
+  };
+
   @action
   handleSubmit(changeset, event) {
     if (event) {
